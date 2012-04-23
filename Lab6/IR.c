563aa1b42b7b0de95e14f5c7b69dc3f3c1e46f18
Jeff Mahler <jmahl42@gmail.com>
2012-04-23 13:57:31 -0500

    Added interpolation to the IR sensors and added a larger delay to the Ping

--- Lab6/IR.c
+++ Lab6/IR.c
@@ -16,8 +16,15 @@
 #define NUM_SENSORS 4
 #define MEDIAN_SIZE 3
 #define IR_FREQ 10
+#define TABLE_SIZE 12
 
 Sema4Type SensorDataAvailable[4];
+
+unsigned short Sensor0Calibration[TABLE_SIZE] = {1023, 855, 585, 450, 355, 310, 230, 185, 158, 125, 105, 0};
+unsigned short Sensor0Measurement[TABLE_SIZE] = {5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 100};
+
+unsigned short Sensor1Calibration[TABLE_SIZE] = {1023, 878, 595, 455, 365, 315, 240, 195, 160, 140, 125, 0};
+unsigned short Sensor1Measurement[TABLE_SIZE] = {5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 100};
 
 unsigned short SensorDistances[4];
 unsigned short S0Values[MEDIAN_SIZE] = {0, };
@@ -34,6 +41,7 @@ void IRSensor1_Handler(unsigned short data);
 void IRSensor2_Handler(unsigned short data);
 void IRSensor3_Handler(unsigned short data);
 unsigned short Median(unsigned short v1, unsigned short v2, unsigned short v3);
+unsigned short Interpolate(unsigned short val, unsigned short* calibration, unsigned short* measurement, int size);
 
 void IR_Init(void) {
   ADCTask tasks[4];
@@ -43,7 +51,8 @@ void IR_Init(void) {
 	tasks[3] = &IRSensor3_Handler; 
 	
 	ADC_CollectSequence(4, IR_FREQ, tasks);
-
+	
+	// initialize semaphores to -2 to ensure that data is not read before the 3rd sample	
 	OS_InitSemaphore(&SensorDataAvailable[0], 0);
 	OS_InitSemaphore(&SensorDataAvailable[1], 0);
 	OS_InitSemaphore(&SensorDataAvailable[2], 0);
@@ -56,16 +65,20 @@ unsigned short IR_GetDistance(int sensor) {
 }
 
 void IRSensor0_Handler(unsigned short data) {
+	unsigned short dist;
   S0Values[S0PutIndex] = data; // overwrite old data if it is there
 	S0PutIndex = (S0PutIndex + 1) % MEDIAN_SIZE;
-  SensorDistances[0] = Median(S0Values[0], S0Values[1], S0Values[2]);
+  dist = Median(S0Values[0], S0Values[1], S0Values[2]);
+	SensorDistances[0] = Interpolate(dist, Sensor0Calibration, Sensor0Measurement, TABLE_SIZE);
 	OS_Signal(&SensorDataAvailable[0]);
 }
 
 void IRSensor1_Handler(unsigned short data) {
+	unsigned short dist;
 	S1Values[S1PutIndex] = data; // overwrite old data if it is there
 	S1PutIndex = (S1PutIndex + 1) % MEDIAN_SIZE;
-  SensorDistances[1] = Median(S1Values[0], S1Values[1], S1Values[2]);
+  dist = Median(S1Values[0], S1Values[1], S1Values[2]);
+	SensorDistances[1] = Interpolate(dist, Sensor1Calibration, Sensor1Measurement, TABLE_SIZE);
 	OS_Signal(&SensorDataAvailable[1]);
 }
 
@@ -94,4 +107,24 @@ unsigned short Median(unsigned short v1, unsigned short v2, unsigned short v3) {
 	  return v2;
 	}
 	return v1;
+}
+
+unsigned short Interpolate(unsigned short val, unsigned short* calibration, unsigned short* measurement, int size) {
+  int x = 1;
+	unsigned short distance;
+	unsigned short slope;
+	unsigned short dx = 0;
+	unsigned short dy = 0;
+
+	while (x < size && calibration[x] < val) {
+	  x++;
+	}
+
+	dx = measurement[x] - measurement[x-1];
+	dy = calibration[x] - calibration[x-1];
+	slope = dy / dx;
+
+	dx = val - calibration[x-1];
+	distance = dx * slope + measurement[x-1];
+	return distance;
 }
